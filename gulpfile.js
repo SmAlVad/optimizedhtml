f73e0a91ed4ba4e8feb7b654bab6309c42e40845
Alex Smirnov <workpost@amurlife.ru>
2018-09-03 14:40:32 +0900

    Added tasks jade, img, fonts, clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-var syntax        = 'scss'; // Syntax: sass or scss;
-
-var gulp          = require('gulp'),
+let gulp          = require('gulp'),
 	gutil         = require('gulp-util' ),
 	sass          = require('gulp-sass'),
 	browserSync   = require('browser-sync'),
@@ -10,6 +8,10 @@ var gulp          = require('gulp'),
 	rename        = require('gulp-rename'),
 	autoprefixer  = require('gulp-autoprefixer'),
 	notify        = require("gulp-notify"),
+	jade 		  = require('gulp-jade'),
+	imagemin      = require('gulp-imagemin'),
+	pngquant      = require('imagemin-pngquant'), 
+	del           = require('del'), 
 	rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
@@ -24,8 +26,17 @@ gulp.task('browser-sync', function() {
 	})
 });
 
-gulp.task('styles', function() {
-	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+gulp.task('html', function() {
+    return gulp.src('assets/jade/**/*.jade')
+        .pipe(jade({
+			pretty: true
+		})) 
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('css', function() {
+	return gulp.src('assets/sass/**/*.sass')
 	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
 	.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['last 15 versions']))
@@ -36,13 +47,13 @@ gulp.task('styles', function() {
 
 gulp.task('js', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/bootstrap.bundle.min.js',
-		'app/js/bootstrap.min.js',
-		'app/js/common.js', // Always at the end
+	//	'assets/libs/jquery/dist/jquery.min.js',
+	//	'assets/js/bootstrap.bundle.min.js',
+	//	'assets/js/bootstrap.min.js',
+		'assets/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
-	.pipe(uglify()) // Mifify js (opt.)
+//	.pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
 	.pipe(browserSync.reload({ stream: true }))
 });
@@ -62,10 +73,34 @@ gulp.task('rsync', function() {
 	}))
 });
 
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	gulp.watch('app/*.html', browserSync.reload)
+gulp.task('img', function() {
+    return gulp.src('assets/img/**') 
+        .pipe(imagemin({  
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant({quality: '65-70', speed: 5})]
+        }))
+        .pipe(gulp.dest('app/img')); 
+});
+
+gulp.task('fonts', function(){
+	return gulp.src('assets/fonts/**')
+	.pipe(gulp.dest('app/font'));
+});
+
+gulp.task('clean', function() {
+    return del.sync('app'); 
+});
+
+gulp.task('build', ['clean', 'html', 'css', 'js', 'img', 'fonts'], function() {
+
+});
+
+gulp.task('watch', ['html','css', 'js', 'browser-sync'], function() {
+	gulp.watch('assets/sass/**/*.sass', ['css']);
+	gulp.watch(['assets/js/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('assets/**/*.jade', ['html']);
 });
 
 gulp.task('default', ['watch']);
